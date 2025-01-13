e5e87cfaa10efdddb08e3483c5088d90493fd308
Samarth Kapoor <kapoorsamarth7@gmail.com>
2025-01-13 22:37:14 +0530

    added clip-paths for scrollTrigger

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,19 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 3;
+  const totalVideos = 4;
   const nextVideoRef = useRef(null);
 
   const handleVideoLoad = () => {
@@ -23,6 +26,12 @@ const Hero = () => {
     setHasClicked(true);
     setCurrentIndex(upcomingVideoIndex);
   }
+
+  useEffect(() => {
+    if(loadedVideos === totalVideos - 1) {
+      setIsLoading(false);
+    }
+  }, [loadedVideos])
 
   useGSAP(() => {
     if(hasClicked) {
@@ -47,10 +56,39 @@ const Hero = () => {
     }
   }, {dependencies: [currentIndex], revertOnUpdate: true})
 
+  useGSAP(() => {
+    gsap.set('#video-frame', {
+      clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+      borderRadius: '0 0 40% 10%'
+    })
+
+    gsap.from('#video-frame', {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      borderRadius: '0 0 0 0',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: '#video-frame',
+        start: 'center center',
+        end: 'bottom center',
+        scrub: true,
+      }
+    })
+  })
+
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
+
+      {isLoading && (
+        <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50'>
+          <div className='three-body'>
+            <div className='three-body__dot' />
+            <div className='three-body__dot' />
+            <div className='three-body__dot' />
+          </div>
+        </div>
+      )}
         <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
             <div>
                 <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
